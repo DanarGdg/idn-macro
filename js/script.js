5f725e4b95835d91c3aff49e5afedd920ec33918
DanarGdg <danargading@gmail.com>
2022-08-26 15:36:31 +0700

    add blog

--- js/script.js
+++ js/script.js
@@ -30,3 +30,23 @@ closeLogo.addEventListener('click', () => {
     logoBlack.classList.toggle('active')
     hamburgerLogo.classList.toggle('active')
 })
+
+// const body = document.body,
+// scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
+// height = scrollWrap.getBoundingClientRect().height - 1,
+// speed = 0.05;
+
+// var offset = 0;
+
+// body.style.height = Math.floor(height) + "px";
+
+// function smoothScroll() {
+//     offset += (window.pageYOffset - offset) * speed;
+
+//     var scroll = "translateY(-" + offset + "px) translateZ(0)";
+//     scrollWrap.style.transform = scroll;
+
+//     callScroll = requestAnimationFrame(smoothScroll);
+// }
+
+// smoothScroll();
