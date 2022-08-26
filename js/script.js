adab33e79e7bfb358f8b7c1fd551e3db14ffcdd8
DanarGdg <danargading@gmail.com>
2022-08-26 11:10:39 +0700

    add js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
-const helloWorld = document.getElementsByTagName('h1');
-console.info(helloWorld);
+const dropdown = document.querySelector('.dropdown')
+const dropdownContent = document.querySelector('.dropdown-content')
+const arrowDropdown = document.querySelector('.arrow-down')
+const hamburgerLogo = document.querySelector('.hamburger-logo')
+const searchLogo = document.querySelector('.search-logo')
+const navbarTextMobile = document.querySelector('.navbar-text-mobile')
+const closeLogo = document.querySelector('.close-logo')
+const logoWhite = document.querySelector('.logo img:nth-child(1)')
+const logoBlack = document.querySelector('.logo img:nth-child(2)')
+
+dropdown.addEventListener('click', () => {
+    arrowDropdown.classList.toggle("active")
+    dropdownContent.classList.toggle("active")
+})
+
+hamburgerLogo.addEventListener('click', () => {
+    searchLogo.classList.toggle('active')
+    navbarTextMobile.classList.toggle('active')
+    closeLogo.classList.toggle('active')
+    logoWhite.classList.toggle('active')
+    logoBlack.classList.toggle('active')
+    hamburgerLogo.classList.toggle('active')
+})
+
+closeLogo.addEventListener('click', () => {
+    searchLogo.classList.toggle('active')
+    navbarTextMobile.classList.toggle('active')
+    closeLogo.classList.toggle('active')
+    logoWhite.classList.toggle('active')
+    logoBlack.classList.toggle('active')
+    hamburgerLogo.classList.toggle('active')
+})
